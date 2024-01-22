115784e0b146c358762fbfa0177cd19868873915
Codrin Popa <codrin@falcontrading.ro>
2024-01-22 17:42:26 +0200

    Create ContactedCandidate and HiredCandidate models and related relationships for Company and Candidate models

--- app/Models/Candidate.php
+++ app/Models/Candidate.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Candidate extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'email'];
+
+    public function contactedCompanies()
+    {
+        return $this->belongsToMany(Company::class, 'contacted_candidates', 'candidate_id', 'company_id');
+    }
+
+    public function hiredCompanies()
+    {
+        return $this->belongsToMany(Company::class, 'hired_candidates', 'candidate_id', 'company_id');
+    }
 }
--- app/Models/Company.php
+++ app/Models/Company.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+
+    public function wallets()
+    {
+        return $this->hasMany(Wallet::class);
+    }
+
+    public function contactedCandidates()
+    {
+        return $this->hasMany(ContactedCandidate::class);
+    }
+
+    public function hiredCandidates(){
+        return $this->hasMany(HiredCandidate::class);
+    }
 }
